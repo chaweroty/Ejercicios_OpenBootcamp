12c323c583127d07bf5b0649208a6d9d5393cb90
Sebastián García <smasgg@gmail.com>
2023-01-02 19:25:40 -0500

    actualizacion

--- Java basico/src/Ejercicio_tema_2/EjercicioTema2.java
+++ Java basico/src/Ejercicio_tema_2/EjercicioTema2.java
@@ -1,4 +1,22 @@
 package Ejercicio_tema_2;
 
 public class EjercicioTema2 {
+
+    public static void main(String[] args) {
+
+        double iva = 0.19;
+        double precio = 1795;
+        double resulado = FunctionIva(precio, iva);
+
+        System.out.println("El precio con iva es:" + resulado + " COP");
+    }
+
+    public static double  FunctionIva(double precio, double iva) {
+
+        double precioConIva = precio * iva;
+
+        double total = precioConIva + precio;
+
+        return total;
+    }
 }
