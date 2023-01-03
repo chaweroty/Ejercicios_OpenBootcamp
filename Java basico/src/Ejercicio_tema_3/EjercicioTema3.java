9b3b09bdc797a189b3df7544c0308c5864774ca4
Sebastián García <smasgg@gmail.com>
2023-01-03 13:45:37 -0500

    Nuevo ejercicio

--- Java basico/src/Ejercicio_tema_3/EjercicioTema3.java
+++ Java basico/src/Ejercicio_tema_3/EjercicioTema3.java
@@ -1,4 +1,14 @@
 package Ejercicio_tema_3;
 
 public class EjercicioTema3 {
+
+    public static void main(String[] args) {
+
+        String [] nombres= {"Santiago", "Felipe", "Maria Camila", "Andrea", "Estefania", "Dora", "Sebastian", "Juanita", "Juan Daniel"};
+
+        for (int i = 0; i < nombres.length; i++) {
+
+            System.out.println("Feliz año para: " + nombres[i]);
+        }
+    }
 }
